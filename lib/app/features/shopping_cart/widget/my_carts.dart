0db91d379441e1d748126dea0f27e61b82fdaf44
lamva112 <lamkhac23102001@gmail.com>
2022-08-19 04:34:29 +0700

    [update] add to cart

--- lib/app/features/shopping_cart/widget/my_carts.dart
+++ lib/app/features/shopping_cart/widget/my_carts.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:food_nija_application/app/change_notifies/cart_provider.dart';
 import 'package:food_nija_application/app/core/utils/size_config.dart';
 import 'package:food_nija_application/app/features/shopping_cart/widget/order_bill.dart';
 import 'package:food_nija_application/app/features/shopping_cart/widget/order_food_card.dart';
 import 'package:food_nija_application/data/models/cart.dart';
 import 'package:food_nija_application/data/models/order.dart';
 import 'package:food_nija_application/data/models/order_detail.dart';
+import 'package:provider/provider.dart';
 
 class MyCarts extends StatefulWidget {
   const MyCarts({Key? key}) : super(key: key);
@@ -16,6 +18,9 @@ class MyCarts extends StatefulWidget {
 class _MyCartsState extends State<MyCarts> {
   @override
   Widget build(BuildContext context) {
+    final cartProvider = Provider.of<CartProvider>(context);
+    final cartItemsList =
+        cartProvider.getCartItems.values.toList().reversed.toList();
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.symmetric(
@@ -30,17 +35,14 @@ class _MyCartsState extends State<MyCarts> {
               child: ListView.builder(
                 shrinkWrap: true,
                 itemBuilder: (BuildContext _, int index) {
-                  var orderFood = listCart[index];
-                  return OrderFoodCard(
-                    cart: orderFood,
-                    onPressed: (context) {
-                      setState(() {
-                        listCart.remove(listCart[index]);
-                      });
-                    },
+                  return ChangeNotifierProvider.value(
+                    value: cartItemsList[index],
+                    child: OrderFoodCard(
+                      q: cartItemsList[index].quantity,
+                    ),
                   );
                 },
-                itemCount: listCart.length,
+                itemCount: cartItemsList.length,
               ),
             ),
             SizedBox(height: getHeight(20)),
--- lib/app/features/shopping_cart/widget/order_food_card.dart
+++ lib/app/features/shopping_cart/widget/order_food_card.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:food_nija_application/app/change_notifies/cart_provider.dart';
+import 'package:food_nija_application/app/change_notifies/foods_provider.dart';
 import 'package:food_nija_application/app/core/utils/size_config.dart';
 import 'package:food_nija_application/app/core/values/app_colors.dart';
 import 'package:food_nija_application/data/models/cart.dart';
+import 'package:food_nija_application/data/models/cart_model.dart';
+import 'package:provider/provider.dart';
 
 class OrderFoodCard extends StatefulWidget {
-  final Function(BuildContext)? onPressed;
-  final Cart cart;
-
-  const OrderFoodCard({Key? key, required this.cart, this.onPressed})
-      : super(key: key);
-
+  final int q;
+  const OrderFoodCard({Key? key, required this.q}) : super(key: key);
   @override
   State<OrderFoodCard> createState() => _OrderFoodCardState();
 }
 
 class _OrderFoodCardState extends State<OrderFoodCard> {
-  late final Cart _cart = widget.cart;
+  final _quantityTextController = TextEditingController();
+  @override
+  void initState() {
+    _quantityTextController.text = widget.q.toString();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final foodsProvider = Provider.of<FoodsProvider>(context);
+    final cartModel = Provider.of<CartModel>(context);
+    final getCurrfood = foodsProvider.findProdById(cartModel.productId);
+    final cartProvider = Provider.of<CartProvider>(context);
     return Card(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(22),
@@ -32,7 +42,13 @@ class _OrderFoodCardState extends State<OrderFoodCard> {
           motion: const StretchMotion(),
           children: [
             SlidableAction(
-              onPressed: widget.onPressed,
+              onPressed: ((context) async => {
+                    await cartProvider.removeOneItem(
+                      cartId: cartModel.id,
+                      productId: cartModel.productId,
+                      quantity: cartModel.quantity,
+                    ),
+                  }),
               backgroundColor: AppColors.priceTextColor,
               foregroundColor: Colors.white,
               icon: Icons.delete,
@@ -44,16 +60,16 @@ class _OrderFoodCardState extends State<OrderFoodCard> {
         child: Padding(
           padding: EdgeInsets.symmetric(vertical: getHeight(10)),
           child: ListTile(
-            // leading: Image.asset(
-            //   _cart.food.image,
-            //   width: getWidth(65),
-            //   height: getWidth(65),
-            // ),
+            leading: Image.network(
+              getCurrfood.imageURL,
+              width: getWidth(65),
+              height: getWidth(65),
+            ),
             title: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  _cart.food.name,
+                  getCurrfood.name,
                   overflow: TextOverflow.ellipsis,
                   style: TextStyle(
                     fontSize: getFont(16),
@@ -62,7 +78,7 @@ class _OrderFoodCardState extends State<OrderFoodCard> {
                   ),
                 ),
                 Text(
-                  _cart.food.description,
+                  getCurrfood.resName,
                   overflow: TextOverflow.ellipsis,
                   style: TextStyle(
                     fontSize: getFont(15),
@@ -73,7 +89,7 @@ class _OrderFoodCardState extends State<OrderFoodCard> {
               ],
             ),
             subtitle: Text(
-              '\$ ${_cart.food.price}',
+              '\$ ${getCurrfood.price}',
               style: TextStyle(
                 fontSize: getFont(17),
                 color: AppColors.primaryColor,
@@ -84,50 +100,103 @@ class _OrderFoodCardState extends State<OrderFoodCard> {
               width: getWidth(110),
               child: Row(
                 children: [
-                  MaterialButton(
-                    onPressed: () {
-                      setState(() {
-                        _cart.quantity--;
-                      });
+                  _quantityController(
+                    fct: () {
+                      if (_quantityTextController.text == '1') {
+                        return;
+                      } else {
+                        cartProvider.reduceQuantityByOne(cartModel.productId);
+                        setState(() {
+                          _quantityTextController.text =
+                              (int.parse(_quantityTextController.text) - 1)
+                                  .toString();
+                        });
+                      }
                     },
-                    color: AppColors.primaryColor.withOpacity(0.1),
-                    padding: EdgeInsets.all(getWidth(10)),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    elevation: 0,
-                    height: getWidth(15),
-                    minWidth: getWidth(15),
-                    child: Icon(Icons.remove,
-                        color: AppColors.primaryColor, size: getWidth(15)),
+                    color: Colors.red,
+                    icon: Icons.remove,
+                    isLeft: true,
                   ),
-                  SizedBox(width: getWidth(4.5)),
-                  Text(
-                    _cart.quantity.toString(),
-                    style: TextStyle(
-                      color: AppColors.textColor,
-                      fontSize: getFont(16),
+                  Flexible(
+                    flex: 1,
+                    child: TextField(
+                      controller: _quantityTextController,
+                      keyboardType: TextInputType.number,
+                      maxLines: 1,
+                      decoration: const InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(),
+                        ),
+                      ),
+                      inputFormatters: [
+                        FilteringTextInputFormatter.allow(
+                          RegExp('[0-9]'),
+                        ),
+                      ],
+                      onChanged: (v) {
+                        setState(() {
+                          if (v.isEmpty) {
+                            _quantityTextController.text = '1';
+                          } else {
+                            return;
+                          }
+                        });
+                      },
                     ),
                   ),
-                  SizedBox(width: getWidth(4.5)),
-                  MaterialButton(
-                    onPressed: () {
-                      setState(() {
-                        _cart.quantity++;
-                      });
+                  _quantityController(
+                    fct: () {
+                      if (_quantityTextController.text == '1') {
+                        return;
+                      } else {
+                        cartProvider.reduceQuantityByOne(cartModel.productId);
+                        setState(() {
+                          _quantityTextController.text =
+                              (int.parse(_quantityTextController.text) - 1)
+                                  .toString();
+                        });
+                      }
                     },
-                    color: AppColors.primaryColor,
-                    padding: EdgeInsets.all(getWidth(10)),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    elevation: 0,
-                    height: getWidth(15),
-                    minWidth: getWidth(15),
-                    child: Icon(Icons.add,
-                        color: AppColors.itemChildColor, size: getWidth(15)),
+                    color: Colors.red,
+                    icon: Icons.remove,
+                    isLeft: true,
                   ),
                 ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _quantityController({
+    required Function fct,
+    required IconData icon,
+    required Color color,
+    required bool isLeft,
+  }) {
+    return Flexible(
+      flex: 2,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        child: Material(
+          color: isLeft
+              ? AppColors.primaryColor.withOpacity(0.1)
+              : AppColors.primaryColor,
+          borderRadius: BorderRadius.circular(12),
+          child: InkWell(
+            borderRadius: BorderRadius.circular(12),
+            onTap: () {
+              fct();
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Icon(
+                icon,
+                color:
+                    isLeft ? AppColors.primaryColor : AppColors.itemChildColor,
+                size: getWidth(15),
               ),
             ),
           ),
